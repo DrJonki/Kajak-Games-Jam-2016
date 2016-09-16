0c6c1264300e0d50993420a408638e9d7f181d96
Kreander Patrick TTI12S <patrickkreander@kamk.fi>
2016-09-17 00:17:46 +0300

    +Rigid Body  +Partially working rear wheel acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,14 @@ public:
 		auto cam = findChild("cam")->getComponent<jop::Camera>();
 		cam->setSize(cam->getSize()*10.f);
 
-		createChild("car")->createComponent<Sprite>(getRenderer()).setSize(glm::vec2(1.f)).setTexture(rm::get<Texture2D>("car.jpg"),false);	
+		createChild("car")->createComponent<Sprite>(getRenderer()).setSize(glm::vec2(1.f,2.f)).
+			setTexture(rm::get<Texture2D>("car.jpg"),false).
+			getObject()->createComponent<RigidBody2D>(getWorld<2>(),RigidBody2D::ConstructInfo2D(rm::getNamed<RectangleShape2D>("car", 1.f,2.f),RigidBody::Type::Dynamic,1.f));
+		getWorld<2>().setGravity(glm::vec2());
+		getWorld<2>().setDebugMode(true);
+
+
+		createChild("debugDot")->createComponent<Sprite>(getRenderer()).setSize(glm::vec2(0.1f));
 	}
 
 	void HandleKey(const int key){
@@ -41,25 +48,67 @@ public:
 			cam->setSize(camSize + glm::vec2(deltaZoom, deltaZoom/(camSize.x/camSize.y)));
 	}
 
+	glm::vec2 slideAccelerator()
+	{
+		auto carObj = findChild("car")->getComponent<jop::RigidBody2D>();
+		glm::vec2 driveDirection = glm::vec2(findChild("car")->getLocalUp());
+		static jop::DynamicSetting<float> linearFriction("game@linearFriction", 200.f);
+		glm::vec2 speedDirection = carObj->getLinearVelocity();
+		float angleBetween = glm::dot(glm::normalize(driveDirection), glm::normalize(speedDirection));
+
+		//(driveDirection.x << ", " << driveDirection.y);
+		//JOP_DEBUG_DIAG(speedDirection.x << ", " << speedDirection.y);
+		JOP_DEBUG_DIAG(angleBetween);
+
+		return speedDirection; // -linearFriction*sin(angleBetween)*speedDirection;
+		
+	}
+
     void preUpdate(const float deltaTime) override
     {
-		auto carObj = findChild("car");
+		auto carObj = findChild("car")->getComponent<jop::RigidBody2D>();
 
+		glm::vec2 driveDirection = glm::vec2(findChild("car")->getLocalUp());
+
+		static jop::DynamicSetting<float> rearAcceleration("game@rearAccel", 100.f);
+		static jop::DynamicSetting<float> rotateTorgue("game@rotateTorgue", 10.f);
+		static jop::DynamicSetting<float> rotationFriction("game@rotationFriction", 200.f);
+		static jop::DynamicSetting<float> linearFriction("game@linearFriction", 200.f);
+
+		
+
+		float angularVelo = carObj->getAngularVelocity();
+		if (angularVelo > 0.04)
+		{
+			carObj->applyTorque(-deltaTime*rotationFriction);
+		}
+		else if (angularVelo < - 0.04)
+		{
+			carObj->applyTorque(deltaTime*rotationFriction);
+		}
+		else
+		{
+			carObj->setAngularVelocity(0);
+		}
+
+		//JOP_DEBUG_DIAG(glm::length(carObj->getLinearVelocity()));
+
+		findChild("debugDot")->setPosition(carObj->getObject()->getLocalPosition() - glm::vec3(driveDirection, 0.f));
 		if (jop::Keyboard::isKeyDown(jop::Keyboard::Up))
 		{
-			carObj->move(0, deltaTime, 0);
+			carObj->applyForce(rearAcceleration.value*glm::normalize(driveDirection)*deltaTime, glm::vec2(carObj->getObject()->getLocalPosition()-glm::vec3(driveDirection, 0.f)));
 		}
 		if (jop::Keyboard::isKeyDown(jop::Keyboard::Down))
 		{
-			carObj->move(0, -deltaTime, 0);
+			carObj->applyCentralForce(-linearFriction.value*glm::normalize(carObj->getLinearVelocity())*deltaTime);
 		}
 		if (jop::Keyboard::isKeyDown(jop::Keyboard::Left))
 		{
-			carObj->move(-deltaTime, 0, 0);
+			carObj->applyTorque(rotateTorgue);
 		}
 		if (jop::Keyboard::isKeyDown(jop::Keyboard::Right))
 		{
-			carObj->move(deltaTime, 0, 0);
+			carObj->applyTorque(-rotateTorgue);
 		}
 		if (jop::Keyboard::isKeyDown(jop::Keyboard::KeypadAdd))
 		{
@@ -69,6 +118,14 @@ public:
 		{
 			zoomCamera(1.f);
 		}
+
+		// sliding makes car go slower
+
+		slideAccelerator();
+
+		//carObj->applyCentralForce(slideAccelerator()*deltaTime);
+		
+
     }
 };
 
